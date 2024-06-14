999c0b63a8e751596eca2d8bc139bd27e845eee6
Akaash Kamdar <akaashkamdar@Akaashs-MacBook-Air.local>
2024-06-14 16:12:53 -0400

    I am updating the cleaned dataset

--- Facebook.R
+++ Facebook.R
@@ -1,3 +1,8 @@
+# Filter out the rows where RAMstorage is 16
+library(magrittr)
 data <- read.csv("~/Downloads/MSDS-Orientation-Computer-Survey(in).csv")
+data = data[data$CPU.Number.of.Cores..int.>=0,]
 hist(data$CPU.Number.of.Cores..int., xlab = "CPU Number of Cores", col = "lightblue", border="black",
      main = "Number of CPU Cores for MSDS Students")
+
+
